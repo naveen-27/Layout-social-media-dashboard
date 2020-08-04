df0b3f4aec49b197997b2310e36c263c806be1b9
Naveen Kumar <456endson@gmail.com>
2020-08-04 09:57:44 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Social Media Dashboard - With Theme Toggle
+
+The page is written in HTML and CSS. Challenge from "Frontendmentor.io".
+
+### Screenshots
+
+<img src="./screenshots/Desktop-light.png">
+<img src="./screenshots/Desktop-dark.png">
+<img src="./screenshots/Mobile-light.png">
+<img src="./screenshots/Mobile-dark.png">
---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+const slide  = document.querySelector('#dot'),
+      toggle = document.querySelector('#theme-toggle'),
+      theme  = document.querySelector('#theme-style'),
+      text   = document.querySelector('.theme > h4');
+
+toggle.addEventListener('click', () => {
+    slide.style.transform = (slide.style.transform === 'translateX(0%)') 
+                             ? 'translateX(100%)' : 'translateX(0%)';
+
+    let color = theme.attributes.href.nodeValue;
+    theme.attributes.href.nodeValue = (color === './stylesheets/light.css') ? './stylesheets/dark.css' : './stylesheets/light.css';
+
+    text.innerText = (text.innerText === 'Light Mode' ? 'Dark Mode' : 'Light Mode');
+});
