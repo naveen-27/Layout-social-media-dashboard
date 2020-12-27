f9d2fd111230ec39d9a706190d93f59d014b5259
Naveen Kumar <456endson@gmail.com>
2020-12-27 13:03:14 +0530

    Refactored theme switching to avoid network calls

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const slide  = document.querySelector('#dot'),
-      toggle = document.querySelector('#theme-toggle'),
-      theme  = document.querySelector('#theme-style'),
-      text   = document.querySelector('.theme > h4');
+const slide = document.querySelector("#dot"),
+  toggle = document.querySelector("#theme-toggle"),
+  theme = document.querySelector("#theme-style"),
+  text = document.querySelector(".theme > h4");
 
-toggle.addEventListener('click', () => {
-    slide.style.transform = (slide.style.transform === 'translateX(0%)') 
-                             ? 'translateX(100%)' : 'translateX(0%)';
+toggle.addEventListener("click", () => {
+  slide.style.transform =
+    slide.style.transform === "translateX(0%)"
+      ? "translateX(100%)"
+      : "translateX(0%)";
 
-    let color = theme.attributes.href.nodeValue;
-    theme.attributes.href.nodeValue = (color === './light.css') ? './dark.css' : './light.css';
+  document.body.dataset.theme =
+    document.body.dataset.theme === "light" ? "dark" : "light";
 
-    text.innerText = (text.innerText === 'Light Mode' ? 'Dark Mode' : 'Light Mode');
+  text.innerText = text.innerText === "Light Mode" ? "Dark Mode" : "Light Mode";
 });
